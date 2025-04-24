212dd2f1ba1a09b1b467686744a55a6de7920dc6
Teresa <teresagalaviza@gmail.com>
2025-04-24 14:19:09 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import TodoRowItem from './components/TodoRowItem';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mt-5 container">
+      <div className="card">
+        <div className="card-header">
+          Your todo's
+        </div>
+        <div className="card-body">
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Description</th>
+                <th scope="col">Assigned</th>
+              </tr>
+            </thead>
+            <tbody>
+              <TodoRowItem />
+              <tr>
+                <th scope="row">2</th>
+                <td>clean house</td>
+                <td>Jose</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/TodoRowItem.js
@@ -0,0 +1,11 @@
+function TodoRowItem() {
+    return (
+        <tr>
+            <th scope="row">1</th>
+            <td>Cook meal</td>
+            <td>María</td>
+        </tr>
+    );
+}
+
+export default TodoRowItem;
